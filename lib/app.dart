35515c40ec501d55ab37684c47f587643f24c971
elfeny7 <ikmalfaris50@gmail.com>
2024-01-15 12:31:51 +0700

    rename some stuff

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+
+class MainApp extends StatelessWidget {
+  final UserRepository userRepository;
+  const MainApp(this.userRepository, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
