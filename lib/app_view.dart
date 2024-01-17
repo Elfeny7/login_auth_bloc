ed6d48729c44c5233081c5e7f6f03fde11a19589
elfeny7 <ikmalfaris50@gmail.com>
2024-01-17 13:42:02 +0700

    feat: working with theme

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_auth_bloc/screens/auth/welcome_screen.dart';
+
+import 'blocs/authentication_bloc/authentication_bloc.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -7,7 +11,28 @@ class MyAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firebase Auth with Bloc',
-      home: Container(),
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          background: Colors.white,
+          onBackground: Colors.black,
+          primary: Color.fromARGB(255, 255, 228, 76),
+          onPrimary: Colors.black,
+          secondary: Color.fromARGB(255, 255, 193, 68),
+          onSecondary: Colors.white,
+          tertiary: Color.fromARGB(255, 255, 123, 52),
+          error: Colors.red,
+          outline: Color.fromARGB(255, 50, 50, 50),
+        ),
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return const HomeScreen();
+          } else {
+            return const WelcomeScreen();
+          }
+        },
+      ),
     );
   }
 }
---
+++ lib/screens/auth/welcome_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(initialIndex: 0, length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(20, -1.2),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
