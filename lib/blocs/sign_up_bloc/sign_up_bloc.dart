7bf3eb689eaa43a79d9ed39031ca4e80791ff506
elfeny7 <ikmalfaris50@gmail.com>
2024-01-26 13:24:15 +0700

    feat: add sign up state and bloc

--- lib/blocs/sign_up_bloc/sign_up_bloc.dart
+++ lib/blocs/sign_up_bloc/sign_up_bloc.dart
@@ -6,9 +6,19 @@ part 'sign_up_event.dart';
 part 'sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc() : super(SignUpInitial()) {
-    on<SignUpEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserRepository _userRepository;
+  SignUpBloc({required UserRepository userRepository})
+      : _userRepository = userRepository,
+        super(SignUpInitial()) {
+    on<SignUpRequired>((event, emit) async {
+      emit(SignUpProcess());
+      try {
+        MyUser user = await userRepository.signUp(event.user, event.password);
+        await _userRepository.setUserData(user);
+        emit(SignUpSuccess());
+      } catch (e) {
+        emit(SignUpFailure());
+      }
     });
   }
 }
