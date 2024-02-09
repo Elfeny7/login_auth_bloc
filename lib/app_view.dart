0a4449a089ec022664be5a2a2ce0666d370e4448
elfeny7 <ikmalfaris50@gmail.com>
2024-02-09 20:51:28 +0700

    feat: add bloc provider home screen

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_auth_bloc/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:login_auth_bloc/screens/auth/welcome_screen.dart';
 
 import 'blocs/authentication_bloc/authentication_bloc.dart';
@@ -28,7 +29,12 @@ class MyAppView extends StatelessWidget {
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return const HomeScreen();
+            return BlocProvider(
+              create: (context) => SignInBloc(
+                  userRepository:
+                      context.read<AuthenticationBloc>().userRepository),
+              child: const HomeScreen(),
+            );
           } else {
             return const WelcomeScreen();
           }
