590deca012afb2ff768aa7fa34e4aa36a87232c0
elfeny7 <ikmalfaris50@gmail.com>
2024-02-23 23:04:02 +0700

    feat: add some text

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome'),
+        title: const Text('Welcome to my app'),
       ),
     );
   }
